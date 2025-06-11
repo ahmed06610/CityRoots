7ff3d17e8d2b98c331deb6e3b121210c7cfae787
ahmad-62 <aamkhalid2011@gmail.com>
2025-06-11 04:52:54 +0300

    Pp
    payment background

---
+++ CityRoots.Core/Services/PaymentBackgroundService.cs
@@ -0,0 +1,40 @@
+﻿using CityRoots.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CityRoots.Core.Services
+{
+    public class PaymentBackgroundService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public PaymentBackgroundService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork=unitOfWork;
+        }
+        public async Task ProcessPaymentsAsync()
+        {
+
+            var pendingPayments = await _unitOfWork.Payment.FindAllAsync(
+       p => p.Statue == "قيد الانتظار" &&
+Microsoft.EntityFrameworkCore.EF.Functions.DateDiffDay(p.PaymentDate, DateTime.UtcNow) > 3
+   );
+
+            foreach (var payment in pendingPayments)
+            {
+                
+                payment.Statue = "مرفوض"; 
+                _unitOfWork.Payment.Update(payment);
+            }
+
+            if (pendingPayments.Any())
+            {
+                await _unitOfWork.CompleteAsync();
+            }
+        }
+    }
+}
